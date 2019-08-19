22f77b0013a3678054a41661be28bbaba464904d
XXXMrG <mrgitservice@outlook.com>
2019-08-19 22:13:12 +0800

    add CSS Module

--- src/routes/App/index.jsx
+++ src/routes/App/index.jsx
@@ -1,20 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react"
 import logo from "./logo.svg"
-import "./App.css"
+import style from "./App.module.css"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={style.App}>
+        <header className={style.header}>
+          <img src={logo} className={style.logo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className={style.link}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
